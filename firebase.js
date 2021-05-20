d1444c9e0fb0cc1e2d524a0a7c42b844e9c99c69
BilalAfsar2021 <83045678+BilalAfsar2021@users.noreply.github.com>
2021-05-19 19:44:57 -0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -7,3 +7,15 @@
     };
     firebase.initializeApp(config);
 </script>
+
+ const messaging = firebase.messaging();
+ messaging
+   .requestPermission()
+   .then(function () {
+     MsgElem.innerHTML = "Notification permission granted." 
+     console.log("Notification permission granted.");
+   })
+   .catch(function (err) {
+   ErrElem.innerHTML = ErrElem.innerHTML + "; " + err
+   console.log("Unable to get permission to notify.", err);
+ });
