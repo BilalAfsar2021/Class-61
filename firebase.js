13a8fa2ab2ac2707c8d45c9da1ba728989faa1b4
BilalAfsar2021 <83045678+BilalAfsar2021@users.noreply.github.com>
2021-05-19 19:30:33 -0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,9 @@
+<script>
+    var config = {
+        messagingSenderId: "595783721675",
+        apiKey: "AIzaSyCO5BHC2O9mhBZtLXl-4AafmHdB22yWbjM",
+        projectId: "web-push-7a445",
+        appId: "1:595783721675:web:4904b8c61dbe05952b714e"
+    };
+    firebase.initializeApp(config);
+</script>
